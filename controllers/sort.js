2c86402757e2872cfac03b432d23732d2b826102
anitta <anittavarghese@riafy.me>
2021-05-17 15:00:45 +0530

    popular

--- controllers/sort.js
+++ controllers/sort.js
@@ -6,7 +6,7 @@ module.exports.sortByNewest = async (req, res, next) => {
     const id = req.params.id;
 
     const products = await Product.find({ category_id: id }).sort({
-      createdAt: "desc",
+      createdDate: "descending",
     });
 
     res.send({
@@ -74,14 +74,23 @@ module.exports.sortByPopular = async (req, res, next) => {
   try {
     const id = req.params.id;
 
-    const products = await Product.find({
-      category_id: id,
-    }).sort({ rating: "desc" });
+    const popularProductsIds = await Product.aggregate([
+      { $unwind: "$review" },
+      { $group: { _id: "$_id", length: { $sum: 1 } } },
+      { $sort: { length: -1 } },
+    ]);
+
+    const products = popularProductsIds.map(async (product) => {
+      const item = await Product.findOne({ _id: product._id });
+      return item;
+    });
+
+    const unpackProducts = await Promise.all(products);
 
     res.send({
       Status: 200,
       Message: "Success",
-      Data: products,
+      Data: unpackProducts,
     });
   } catch (err) {
     console.log(err);
